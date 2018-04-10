d9d492f36d44c30bb19221eb3c6059afe2da7c1a
Serhii Matviienko <s-smatviienko@aras.com>
2018-04-10 20:33:28 +0300

    Feature: initial structure for working on Tellure (Timesiriest Generator)

---
+++ Tellure.Lib/LorenzSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tellure.Lib
+{
+    class LorenzSystem
+    {
+        private double Sigma { get; set; }
+        private double r { get; set; }
+        private double b { get; set; }
+
+        public LorenzSystem(double sigma, double r, double b)
+        {
+	        this.Sigma = sigma;
+	        this.r = r;
+	        this.b = b;
+        }
+
+        public double X(double x, double y)
+        {
+	        return Sigma * (y - x);
+        }
+
+        public double Y(double x, double y, double z)
+        {
+	        return x * (r - z) - y;
+        }
+
+        public double Z(double x, double y, double z)
+        {
+	        return x * y - b * z;
+        }
+	}
+}
