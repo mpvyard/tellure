b0916aacda264ed15f5679fd5fbdabb7c37e296c
Serhii Matviienko <s-smatviienko@aras.com>
2018-04-10 20:33:28 +0300

    Feature: initial structure for working on Tellure (Timesiriest Generator)

---
+++ Tellure.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace Tellure.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tellure.Lib/TimeSeriesGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Tellure.Lib
+{
+    class TimeSeriesGenerator
+    {
+
+        public TimeSeriesGenerator(LorenzSystem system)
+        {
+
+        }
+        public void Generate(Vector<double> y0, double step, int count)
+        {
+            
+        }
+    }
+}
