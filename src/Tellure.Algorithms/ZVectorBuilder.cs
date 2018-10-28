f879f673de6ab0d32c3df9e1bd66b1d92ffdea2d
Serhii Matviienko <smatvienko@outlook.com>
2018-10-28 20:45:59 +0100

    Fix: started implementing solution without ZVectors

--- src/Tellure.Algorithms/ZVectorBuilder.cs
+++ src/Tellure.Algorithms/ZVectorBuilder.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
 using Tellure.Entities;
 
 namespace Tellure.Algorithms
 {
 	public static class ZVectorBuilder
 	{
-
+        [Obsolete]
 		public static IReadOnlyList<ZVector> Build(IReadOnlyList<float> points, int[] step, int firstNumber)
 		{
 			int n = points.Count;
@@ -28,5 +30,32 @@ namespace Tellure.Algorithms
 			}
 			return zVectors;
 		}
+
+        public static IReadOnlyList<Vector<float>> BuildVector(IReadOnlyList<float> points, int[] step, int firstNumber)
+        {
+            int n = points.Count;
+            int z = step.Length + 1;
+
+            var zVectors = new List<Vector<float>>();
+            int k;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                k = i;
+                var cur = new List<float>();
+                for (int j = 0; j < z && k < n; j++)
+                {
+                    cur.Add(points[k]);
+                    k += step[j % step.Length];
+                }
+                if (cur.Count == z)
+                {
+                    zVectors.Add(new Vector<float>(cur.ToArray()));
+                }
+            }
+
+            return zVectors;
+        }
     }
 }
--- src/Tellure.Algorithms/ZVectorBuilder3d.cs
+++ src/Tellure.Algorithms/ZVectorBuilder3d.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 using Tellure.Entities;
 
@@ -7,6 +8,7 @@ namespace Tellure.Algorithms
     public static class ZVectorBuilder3d
     {
         //For Vector3
+        [Obsolete]
         public static IReadOnlyList<ZVector3d> Build(IReadOnlyList<Vector3> points, int[] step, int firstNumber)//firstNumber == series.Points.Count ???
         {
             int n = points.Count;
@@ -28,6 +30,33 @@ namespace Tellure.Algorithms
                     zVectors.Add(new ZVector3d(cur, step, firstNumber++));
                 }
             }
+            return zVectors;
+        }
+
+        public static IReadOnlyList<Vector<Vector3>> BuildVector(IReadOnlyList<Vector3> points, int[] step, int firstNumber)
+        {
+            int n = points.Count;
+            int z = step.Length + 1;
+
+            var zVectors = new List<Vector<Vector3>>();
+            int k;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                k = i;
+                var cur = new List<Vector3>();
+                for (int j = 0; j < z && k < n; j++)
+                {
+                    cur.Add(points[k]);
+                    k += step[j % step.Length];
+                }
+                if (cur.Count == z)
+                {
+                    zVectors.Add(new Vector<Vector3>(cur.ToArray()));
+                }
+            }
+
             return zVectors;
         }
     }
